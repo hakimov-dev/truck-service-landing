92347fea286bed397f1ebf1af07335f572c06c98
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 22:48:35 +0500

    Added content in content component.

---
+++ src/components/Content/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+      <h1>This is Content component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
