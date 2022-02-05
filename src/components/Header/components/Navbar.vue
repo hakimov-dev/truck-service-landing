77aac7d04de3f294ff6a496a35bb29d9901bdd72
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 15:25:58 +0500

    Errors found and corrected.

--- src/components/Header/components/Navbar.vue
+++ src/components/Header/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
-    <div class="navbar flex justify-betwen items-center">
+    <div class="navbar flex justify-between items-center">
        <h1 class="logo">Logo</h1>
        <div class="links">
            <a href="#" class="link">Asosiy</a>
            <a href="#weWork" class="link">Biz qanday ishlaymiz</a>
            <a href="#weWhatDo" class="link">Biz nima qilamiz</a>
            <a href="#about" class="link">Biz haqimizda</a>
-           <a href="#contact"></a>
+           <a href="#contact">Aloqaga chiqish</a>
        </div>
     </div>
     </div>
