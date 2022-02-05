6911a590f602d14df125ed78f5428ba413141659
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 22:50:52 +0500

    Import content in Landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,13 +1,15 @@
 <template>
    <Header />
+   <Content />
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Content from '@/components/Content'
 export default {
   name: 'Landing',
   components: {
-    Header
+    Header, Content
   }
 }
 </script>
