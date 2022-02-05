2d38022aa9100201d8d258b82588ce8ba029523f
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 00:41:13 +0500

    Update.

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -9,10 +9,6 @@
 
 <script>
 import Card from './components/Card'
-import card1 from './assets/img/card-1.png'
-import card2 from './assets/img/card-2.png'
-import card3 from './assets/img/card-3.png'
-import card4 from './assets/img/card-4.png'
 export default {
   name: 'Content',
   components: {
@@ -23,17 +19,17 @@ export default {
     return {
       cards: [
         {
-          img: '@/assets/img/card-1.png',
+          img: require('@/assets/img/card-1.png'),
           title: 'Biz og’ir yuklaringizni olamiz',
           body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida'
         },
         {
-          img: '@/assets/img/card-2',
+          img: require('@/assets/img/card-2.png'),
           title: 'Tezkor va xafsiz olib boramiz',
           body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida'
         },
         {
-          img: '@/assets/img/card-1',
+          img: require('@/assets/img/card-3.png'),
           title: 'Aytilgan joyga eltamiz',
           body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida'
         }
