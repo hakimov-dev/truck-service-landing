b45600238cb058fd1016b0f3eaae943de7459682
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 16:58:36 +0500

    Pictures posted

--- src/components/Header/components/Content.vue
+++ src/components/Header/components/Content.vue
@@ -1,4 +1,6 @@
 <template>
+   <img src="@/assets/img/header-1.png" class="header-1" alt="404">
+   <img src="@/assets/img/header-2.png" class="header-2" alt="404">
     <div class="container">
       <h1>This is headerContent</h1>
     </div>
