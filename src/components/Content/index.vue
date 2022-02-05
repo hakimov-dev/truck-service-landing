1be280ce70fb8a52d3f1db939b4c9b00fb7fd4e8
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 23:11:29 +0500

    Created 'cards' element.

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="weWork" class="main-content container">
     <h1 class="title text-4xl font-extrabold py-8">Biz qanday ishlaymiz?</h1>
+    <div class="cards">
+    </div>
     </div>
 </template>
 
