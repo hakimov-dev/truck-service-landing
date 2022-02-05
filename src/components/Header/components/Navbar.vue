8369914047ea2d76611a74a345df5c187c44bcdf
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 15:13:39 +0500

    Added links

--- src/components/Header/components/Navbar.vue
+++ src/components/Header/components/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-       <h1>This is navbar component!</h1>
+    <div class="container">
+    <div class="navbar flex justify-betwen items-center">
+       <h1 class="logo">Logo</h1>
+       <div class="links">
+           <a href="#" class="link">Asosiy</a>
+           <a href="#weWork" class="link">Biz qanday ishlaymiz</a>
+           <a href="#weWhatDo" class="link">Biz nima qilamiz</a>
+           <a href="#about" class="link">Biz haqimizda</a>
+           <a href="#contact"></a>
+       </div>
+    </div>
     </div>
 </template>
 
