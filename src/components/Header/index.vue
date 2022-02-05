c73a1d30a5d7e0bd9be23cf6b69741df0f7be879
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 17:41:10 +0500

    Import 'Slider' component.

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,15 +1,17 @@
 <template>
        <Navbar/>
        <Content/>
+       <Slider/>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
 import Content from './components/Content'
+import Slider from './components/Slider'
 export default {
   name: 'Header',
   components: {
-    Navbar, Content
+    Navbar, Content, Slider
   }
 }
 </script>
