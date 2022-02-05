90618ba65e34bb90100faedf423693eb92712be6
Muhammadamin <webhacker0100@gmail.com>
2022-02-05 16:29:53 +0500

    Added content in 'Content'

---
+++ src/components/Header/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content">
+      <h1>This is headerContent</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContent'
+}
+</script>
+
+<style scoped>
+</style>
