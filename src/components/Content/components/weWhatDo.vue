14b729ad5db5237f9728ab4cebda80f6316cfad1
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 10:36:14 +0500

    Added new content in 'weWhatDo' component.

---
+++ src/components/Content/components/weWhatDo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container" id="weWhatDo">
+      <h1>This is weWhatDo component.</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'weWhatDo'
+}
+</script>
+
+<style scoped>
+
+</style>
