b271c2730cf0c9fe3838dbee3487e229123dd3a3
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 12:51:16 +0500

    Added images.

--- src/components/About/index.vue
+++ src/components/About/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container" id="about">
          <div class="content1">
-
+           <img class="content1" src="@/assets/img/content-2.1.png" alt="">
+           <img class="content2" src="@/assets/img/content-2.2.png" alt="">
+           <img class="content3" src="@/assets/img/content-2.3.png" alt="">
          </div>
          <div class="content2"></div>
     </div>
