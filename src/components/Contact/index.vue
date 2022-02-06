654fe603ac54e8415c13488e1597e5080dc8e49c
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 13:44:47 +0500

    Added content in 'Contact' component.

---
+++ src/components/Contact/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+       <h1>This is Contact component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+
+<style scoped>
+
+</style>
