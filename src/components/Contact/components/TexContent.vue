0cf8edde69d6d0cdadc0e35e5b6e8341b2b659e8
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:27:59 +0500

    Added br

--- src/components/Contact/components/TexContent.vue
+++ src/components/Contact/components/TexContent.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="main w-3/4">
     <span class="text-2xl">SAVOL VA TAKLIFLAR BORMI?</span>
+    <br>
           <h1 class="text-6xl font-bold">Biz bilan bog’laning</h1>
           <br>
           <p class="leading-8 w-1/4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida</p>
