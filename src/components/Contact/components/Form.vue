6b6c8710cb86e57472b390fd16dc1dfa76dbdb52
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:34:52 +0500

    Added content ind form

--- src/components/Contact/components/Form.vue
+++ src/components/Contact/components/Form.vue
@@ -1,6 +1,11 @@
 <template>
     <form>
-     <h1>This is Form component!</h1>
+     <label for="name">Ism, Familya</label>
+     <input type="text" id="name" placeholder="To’liq ismingizni kiriting">
+     <label for="phone">Telefon raqam</label>
+     <input type="phone" placeholder="Telefon raqamingizni kiriting">
+     <label for="message">Qo’shimcha xabar (Ixtiyoriy)</label>
+     <textarea id="message" cols="30" rows="10"></textarea>
     </form>
 </template>
 
