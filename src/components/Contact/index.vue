a42a4e42a62825f80120f64844919c6fb25ff928
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:05:56 +0500

    import new 'Form' component.

--- src/components/Contact/index.vue
+++ src/components/Contact/index.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="container">
        <TextContent />
+       <Form />
     </div>
 </template>
 
 <script>
 import TextContent from './components/TexContent'
+import Form from './components/Form'
 export default {
   name: 'Contact',
   components: {
-    TextContent
+    TextContent, Form
   }
 }
 </script>
