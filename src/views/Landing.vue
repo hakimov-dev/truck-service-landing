6fcef058837ea4d7b28a75814a38f1530fc3db09
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 16:26:06 +0500

    Import footer component.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,6 +3,7 @@
    <Content />
    <About />
    <Contact />
+   <Footer />
 </template>
 
 <script>
@@ -10,10 +11,11 @@ import Header from '@/components/Header'
 import Content from '@/components/Content'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 export default {
   name: 'Landing',
   components: {
-    Header, Content, About, Contact
+    Header, Content, About, Contact, Footer
   }
 }
 </script>
