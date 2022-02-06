370a1f628df1da77f03d0a0b32a2ef7bd21678cd
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 17:48:09 +0500

    Added content.

---
+++ src/components/Footer/components/FooterEnd.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container flex justif-between items-center">
+      <h1>Copyright 2021, all right reserved</h1>
+      <h1>Developed by <a target="_blank" href="https://hakimov.netlify.com/" class="font-bold">&copy; Hakimov-dev </a></h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterEnd'
+}
+</script>
+
+<style scoped>
+
+</style>
