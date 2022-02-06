ea842a249b9029890a2a384ceac6fc2bc38d0e46
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:02:18 +0500

    Added content. In 'TextContent' component.

---
+++ src/components/Contact/components/TexContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">
+     <h1>This is TextContent component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TextContent'
+}
+</script>
+
+<style scoped>
+
+</style>
