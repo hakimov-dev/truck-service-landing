b38481144f48f57d4a10ddb9bf68ef4f014889cd
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 10:54:34 +0500

    Added new image

--- src/components/Content/components/weWhatDo.vue
+++ src/components/Content/components/weWhatDo.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="img-container">
-     <!-- <img class="cardsBottom1" src="@/assets/img/cards-bottom1.png" alt=""> -->
-     <!-- <img class="cardsBottom2" src="@/assets/img/cards-bottom2.png" alt=""> -->
+     <img class="cardsBottom2" src="@/assets/img/cards-bottom1-2.png" alt="">
      <img class="cardsBottom3" src="@/assets/img/cards-bottom3.png" alt="">
     </div>
     <div class="container" id="weWhatDo">
