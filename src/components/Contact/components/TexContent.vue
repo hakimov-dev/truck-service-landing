da4d3f91aebcb5bafdd7ca6809f79f33354b55d7
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:25:37 +0500

    Added content.

--- src/components/Contact/components/TexContent.vue
+++ src/components/Contact/components/TexContent.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="main">
-     <h1>This is TextContent component!</h1>
+    <div class="main w-3/4">
+    <span class="text-2xl">SAVOL VA TAKLIFLAR BORMI?</span>
+          <h1 class="text-6xl font-bold">Biz bilan bog’laning</h1>
+          <br>
+          <p class="leading-8 w-1/4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida</p>
     </div>
 </template>
 
