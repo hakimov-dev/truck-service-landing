a725b926d609e81d7446ae1a955249042c1622e6
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 15:32:21 +0500

    Update

--- src/components/Contact/components/TexContent.vue
+++ src/components/Contact/components/TexContent.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="main w-3/4">
     <span class="text-2xl">SAVOL VA TAKLIFLAR BORMI?</span>
-    <br>
-          <h1 class="text-6xl font-bold">Biz bilan bog’laning</h1>
-          <br>
+          <h1 class="text-6xl font-bold py-8">Biz bilan bog’laning</h1>
           <p class="leading-8 w-1/4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in facilisi praesent at magna. In pellentesque hac eget gravida</p>
           <img class="pt-24" src="@/assets/img/arrow.png" alt="">
     </div>
@@ -16,5 +14,12 @@ export default {
 </script>
 
 <style scoped>
-
+  .main{
+      width: 1130px;
+      height: 640px;
+      background: #F5F9FF;
+      padding: 30px;
+      padding-left: 100px ;
+      margin-left: -35px;
+  }
 </style>
