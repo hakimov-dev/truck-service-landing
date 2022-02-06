146b9f97731c1cca94eb8478113031a50b8c01ed
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 11:16:30 +0500

    Added content.

--- src/components/Content/components/weWhatDo.vue
+++ src/components/Content/components/weWhatDo.vue
@@ -4,7 +4,14 @@
      <img class="cardsBottom3" src="@/assets/img/cards-bottom3.png" alt="">
     </div>
     <div class="container" id="weWhatDo">
-      <div class="content-1"></div>
+      <div class="content-1 w-3/5">
+          <span>UMUMAN OLGANDA</span>
+          <h1>Biz nima qilamiz?</h1>
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pellentesque cursus arcu tincidunt nam. Dignissim nec nec facilisis vel, elit, purus fermentum. Cras metus, egestas tortor, nisi, at est facilisis ac feugiat. Eu.</p>
+          <br>
+          <button class="main-btn">Aloqaga chiqish <i class="fas fa-chevron-right"></i></button>
+      </div>
       <div class="content-2"></div>
     </div>
 </template>
