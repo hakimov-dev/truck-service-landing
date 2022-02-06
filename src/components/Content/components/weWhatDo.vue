3d858bb03c727d01a3b6dc02a787f3cee1d8dd37
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 11:08:30 +0500

    Update position.

--- src/components/Content/components/weWhatDo.vue
+++ src/components/Content/components/weWhatDo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="img-container flex justify-between items-center">
+    <div class="img-container">
      <img class="cardsBottom2" src="@/assets/img/cards-bottom1-2.png" alt="">
      <img class="cardsBottom3" src="@/assets/img/cards-bottom3.png" alt="">
     </div>
@@ -15,7 +15,15 @@ export default {
 </script>
 
 <style scoped>
-  /* .img-container{
-
-  } */
+  .cardsBottom3{
+    position: absolute;
+    right: -0px;
+   }
+   .cardsBottom2{
+       position: absolute;
+       left: -0px;
+       width: 300px;
+       object-fit: cover;
+       object-position: center center;
+   }
 </style>
