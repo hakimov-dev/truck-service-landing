661c0cc24b166b0d68e1ebbe01e29fb166b68d04
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 10:43:36 +0500

    Added images.

--- src/components/Content/components/weWhatDo.vue
+++ src/components/Content/components/weWhatDo.vue
@@ -1,5 +1,7 @@
 <template>
-
+     <img class="cardsBottom1" src="@/assets/img/cards-bottom1.png" alt="">
+     <img class="cardsBottom2" src="@/assets/img/cards-bottom2.png" alt="">
+     <img class="cardsBottom3" src="@/assets/img/cards-bottom3.png" alt="">
     <div class="container" id="weWhatDo">
       <h1>This is weWhatDo component.</h1>
     </div>
