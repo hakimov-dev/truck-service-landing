8cb6be8e2c5501cc803bba6eb27ea681292da39e
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 12:06:56 +0500

    Import About component.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,15 +1,17 @@
 <template>
    <Header />
    <Content />
+   <About />
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Content from '@/components/Content'
+import About from '@/components/About'
 export default {
   name: 'Landing',
   components: {
-    Header, Content
+    Header, Content, About
   }
 }
 </script>
