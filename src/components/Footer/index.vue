7878ae258480249d40b5d0921a2d66655df01e87
Muhammadamin <webhacker0100@gmail.com>
2022-02-06 16:21:07 +0500

    Added content in  footer

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+       <h1>This is Footer component!</h1>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
