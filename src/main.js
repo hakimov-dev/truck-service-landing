dfd8e2d84231192281accf2c9e9b81704a78cd1c
Muhammadamin <webhacker0100@gmail.com>
2022-02-08 23:16:19 +0500

    Download 'i18n' library.

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/main.css'
+import i18n from './i18n'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(i18n).use(store).use(router).mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'uz',
+      fallbackLocale: 'ru',
+      localeDir: 'locales',
+      enableLegacy: true,
+      runtimeOnly: false,
+      compositionOnly: true,
+      fullInstall: true
+    }
+  }
+}
