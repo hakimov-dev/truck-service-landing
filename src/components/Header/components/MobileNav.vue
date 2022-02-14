e6d232bc9be7aa2671a1045dc396d9d2ad929aaa
Muhammadamin <webhacker0100@gmail.com>
2022-02-15 00:11:18 +0500

    Added style new style

--- src/components/Header/components/MobileNav.vue
+++ src/components/Header/components/MobileNav.vue
@@ -13,5 +13,14 @@ export default {
 </script>
 
 <style scoped>
-
+  .sidenav{
+      background: transparent ;
+      position: absolute;
+      width: 91%;
+      padding: 30px 0px;
+      z-index: 111;
+      font-weight: 600;
+     background: rgb(2,0,36);
+     background: linear-gradient(252deg, rgba(2,0,36,1) 0%, rgba(180,180,180,0.06206232492997199) 0%, rgba(224, 232, 245, 0.836) 0%);
+ }
 </style>
