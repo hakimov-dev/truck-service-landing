b0a592b8c308d1f44a0e5be79b9f86c32b9bdf79
Muhammadamin <webhacker0100@gmail.com>
2022-02-14 21:07:58 +0500

    Created new parrent div

--- src/components/Header/components/Navbar.vue
+++ src/components/Header/components/Navbar.vue
@@ -3,11 +3,13 @@
     <div class="navbar flex justify-between items-center">
        <h1 class="logo">Logo</h1>
        <div class="links w-2/4 flex justify-between items-center">
+        <div class="desktop-nav">
            <a href="#" class="link">Asosiy</a>
            <a href="#weWork" class="link">Biz qanday ishlaymiz</a>
            <a href="#weWhatDo" class="link">Biz nima qilamiz</a>
            <a href="#about" class="link">Biz haqimizda</a>
            <a href="#contact" class="main-btn">Aloqaga chiqish <i class="fas fa-chevron-right"></i></a>
+        </div>
        </div>
     </div>
     </div>
