3956d4848fece62fda6bc6e90b2747586e97e35a
Muhammadamin <webhacker0100@gmail.com>
2022-02-14 23:15:03 +0500

    Added links.

--- src/components/Header/components/MobileNav.vue
+++ src/components/Header/components/MobileNav.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="sidenav">
-       
+     <a href="#weWork" class="link">Biz qanday ishlaymiz</a>
+     <a href="#weWhatDo" class="link">Biz nima qilamiz</a>
+     <a href="#contact" class="link">Aloqa</a>
     </div>
 </template>
 
